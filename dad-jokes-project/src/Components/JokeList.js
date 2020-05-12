9b314a5bbde6a48b21bd426986301ccbe1fc0b27
Matt Phillips <49458212+Emphillips@users.noreply.github.com>
2020-05-12 12:00:05 -0500

    Updated Joke Project
    Created joke list to display. No styling done yet.

--- dad-jokes-project/src/Components/JokeList.js
+++ dad-jokes-project/src/Components/JokeList.js
@@ -3,16 +3,34 @@ import axios from "axios";
 
 
 class JokeList extends Component {
+    static defaultProps = {
+        numJokesToGet: 10
+    };
+    constructor(props){
+        super(props);
+        this.state = { jokes: [] }; // Jokes place holder
+    }
     async componentDidMount(){
-        // Load Jokes
-    let res = await axios.get("https://icanhazdadjoke.com/");
-    console.log(res);
+    // Load Jokes
+    let jokes = [];
+    while(jokes.length < this.props.numJokesToGet) {
+        let res = await axios.get("https://icanhazdadjoke.com/", {
+            headers: {Accept: "application/json"}  // Header added to format in JSON 
+        });
+        jokes.push(res.data.joke);
+    }
+    this.setState({jokes: jokes});
     }
     
     render() {
-        return (
-            <div>
+        return ( // Jokes display
+            <div className="JokeList">
                 <h1>Dad Jokes</h1>
+                <div className="JokeList-jokes">
+                    {this.state.jokes.map(j => (
+                        <div>{j}</div>
+                    ))}
+                </div>
             </div>
             
         );
